58c77dbe2a757909cb7ca0d58edbf8470fda87d9
Meczner Dávid <dmeczner@gmail.com>
2025-03-25 18:57:51 +0100

    Add project files.

---
+++ EmployeeManagementSystem/Model/Employee.cs
@@ -0,0 +1,99 @@
+﻿using System.ComponentModel;
+
+namespace EmployeeManagementSystem.Model
+{
+    public class Employee : INotifyPropertyChanged
+    {
+        private int _id;
+        private string _name;
+        private Role _role;
+        private string _email;
+        private DateTime _birthDay;
+        private string _birthPlace;
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    OnPropertyChanged(nameof(Id));
+                }
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public Role Role
+        {
+            get => _role;
+            set
+            {
+                if (_role != value)
+                {
+                    _role = value;
+                    OnPropertyChanged(nameof(Role));
+                }
+            }
+        }
+
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                if (_email != value)
+                {
+                    _email = value;
+                    OnPropertyChanged(nameof(Email));
+                }
+            }
+        }
+
+        public DateTime BirthDay
+        {
+            get => _birthDay;
+            set
+            {
+                if (_birthDay != value)
+                {
+                    _birthDay = value;
+                    OnPropertyChanged(nameof(BirthDay));
+                }
+            }
+        }
+
+        public string BirthPlace
+        {
+            get => _birthPlace;
+            set
+            {
+                if (_birthPlace != value)
+                {
+                    _birthPlace = value;
+                    OnPropertyChanged(nameof(BirthPlace));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ EmployeeManagementSystem/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,106 @@
+﻿using EmployeeManagementSystem.Common;
+using EmployeeManagementSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EmployeeManagementSystem.ViewModel
+{
+    public class EmployeeViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Employee> employees;
+        private Employee selectedEmployee;
+        private string searchQuery;
+
+        public ObservableCollection<Employee> Employees
+        {
+            get => employees;
+            set
+            {
+                if (employees != value)
+                {
+                    employees = value;
+                    OnPropertyChanged(nameof(Employees));
+                }
+            }
+        }
+
+        public Employee SelectedEmployee
+        {
+            get => selectedEmployee;
+            set
+            {
+                if (selectedEmployee != value)
+                {
+                    selectedEmployee = value;
+                    OnPropertyChanged(nameof(SelectedEmployee));
+                }
+            }
+        }
+
+        public string SearchQuery
+        {
+            get => searchQuery;
+            set
+            {
+                if (searchQuery != value)
+                {
+                    searchQuery = value;
+                    OnPropertyChanged(nameof(SearchQuery));
+                    SearchEmployees();
+                }
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        public ICommand EditCommand { get; }
+        public ICommand DeleteCommand { get; }
+        public ICommand SearchCommand { get; }
+
+        public EmployeeViewModel()
+        {
+            Employees = new ObservableCollection<Employee>();
+            AddCommand = new RelayCommand(AddEmployee);
+            EditCommand = new RelayCommand(EditEmployee, CanEditOrDelete);
+            DeleteCommand = new RelayCommand(DeleteEmployee, CanEditOrDelete);
+            SearchCommand = new RelayCommand(SearchEmployees);
+        }
+
+        private void AddEmployee()
+        {
+            // Add employee logic
+        }
+
+        private void EditEmployee()
+        {
+            // Edit employee logic
+        }
+
+        private void DeleteEmployee()
+        {
+            // Delete employee logic
+        }
+
+        private bool CanEditOrDelete()
+        {
+            return SelectedEmployee != null;
+        }
+
+        private void SearchEmployees()
+        {
+            // Search employees logic
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
